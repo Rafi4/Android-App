7610156a619b2157e092088b4363fefcbae0b6ab
Rafał Jeroś <50295614+Rafi4@users.noreply.github.com>
2021-03-08 14:19:32 +0100

    Update ShareViewModel.java

--- app/src/main/java/com/example/swipe2/ViewModel/ShareViewModel.java
+++ app/src/main/java/com/example/swipe2/ViewModel/ShareViewModel.java
@@ -1,8 +1,6 @@
 package com.example.swipe2.ViewModel;
-
 import android.graphics.drawable.Drawable;
 import android.util.Log;
-
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -10,7 +8,6 @@ import androidx.lifecycle.ViewModel;
 public class ShareViewModel extends ViewModel {
 
     private MutableLiveData<Drawable> image = new MutableLiveData<>();
-
     public void setImage(Drawable drawable) {
         Log.i("TAG", image+"SHAREDVIEWMODEL setImage it's work");
         image.setValue(drawable);
@@ -20,5 +17,4 @@ public class ShareViewModel extends ViewModel {
         Log.i("TAG", "SHAREDVIEWMODEL getImage it's work");
         return image;
     }
-
 }
